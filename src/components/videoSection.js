dd46b82bf9be61481fad9e28924b5d88d5fea82c
kenput3r <kenput3r@gmail.com>
2019-10-23 07:08:43 -0700

    add new videos

--- src/components/videoSection.js
+++ src/components/videoSection.js
@@ -11,10 +11,18 @@ const VideoSection = () => {
       </div>
       <div className="row multi-video">
         <div className="col s12 l6">
-          <YouTubeVideo url={"https://www.youtube.com/embed/KLjEKpxRwpw?rel=0"} title={"Endorsed by Sal Tinajero"} />
+          <YouTubeVideo url={"https://www.youtube.com/embed/EPaM1bx6nkc?rel=0"} title={"Beatriz on Homelessness in Santa Ana"} />
         </div>
         <div className="col s12 l6">
+          <YouTubeVideo url={"https://www.youtube.com/embed/KLjEKpxRwpw?rel=0"} title={"Endorsed by Sal Tinajero"} />
+        </div>
+      </div>
+      <div className="row multi-video">
+        <div className="col s12 l6">
           <YouTubeVideo url={"https://www.youtube.com/embed/rIvzL2VH43E?rel=0"} title={"Endorsed by Millenials"} />
+        </div>
+        <div className="col s12 l6">
+          <YouTubeVideo url={"https://www.youtube.com/embed/lE24Iit_gqM?rel=0"} title={"Endorsed by Santa Ana Business Owners"} />
         </div>
       </div>
     </div>
